c586dc896db2ed3a2777f2b078b1a18a1fc9cdb8
Junior <zimkhandiaye@gmail.com>
2022-06-24 22:41:43 +0000

    Add some update to the reducer

--- src/app/ngRx/apartment/apartment.reducer.ts
+++ src/app/ngRx/apartment/apartment.reducer.ts
@@ -27,10 +27,55 @@ export function apartmentReducer ( state = initState, action: Action): Apartment
             return {... state, dataState: StateEnumApp.LOADING}
         case   ApartmentActionsTypes.GET_ALL_APARTMENT_SUCCESS:
             return {... state, dataState: StateEnumApp.LOADED, apartments:(<ApartmentActions>action).payload}
-
         case   ApartmentActionsTypes.GET_ALL_APARTMENT_ERROR:
              return {... state, dataState: StateEnumApp.ERROR, apartments:(<ApartmentActions>action).payload}
     
+      
+
+        case ApartmentActionsTypes.CREATE_APARTMENT:
+            return { ...state, dataState: StateEnumApp.LOADING}
+        case ApartmentActionsTypes.CREATE_APARTMENT_SUCCESS:
+             let apps : Apartment[] = [... state.apartments];
+             apps.push((<ApartmentActions>action).payload)
+            return {...state, dataState:StateEnumApp.LOADED, apartments:apps}
+        case ApartmentActionsTypes.CREATE_APARTMENT_ERROR:
+            return {... state, dataState: StateEnumApp.ERROR, apartments:(<ApartmentActions>action).payload}
+
+        case ApartmentActionsTypes.GET_ALL_APARTMENT_BY_CITY:
+             return {... state, dataState: StateEnumApp.LOADING}
+        case ApartmentActionsTypes.GET_ALL_APARTMENT_BY_CITY_SUCCESS:
+            return {... state, dataState: StateEnumApp.LOADED, apartments:(<ApartmentActions>action).payload}
+        case ApartmentActionsTypes.GET_ALL_APARTMENT_BY_CITY_ERROR:
+            return {... state, dataState: StateEnumApp.ERROR, apartments:(<ApartmentActions>action).payload}
+
+        
+        case ApartmentActionsTypes.GET_ONE_APARTMENT:
+            return {... state, dataState: StateEnumApp.LOADING}
+        case ApartmentActionsTypes.GET_ONE_APARTMENT_SUCCESS:
+            return {... state, dataState: StateEnumApp.LOADED, apartments:(<ApartmentActions>action).payload}
+        case ApartmentActionsTypes.GET_ONE_APARTMENT_ERROR:
+            return {... state, dataState: StateEnumApp.ERROR, apartments:(<ApartmentActions>action).payload}
+        
+
+        case ApartmentActionsTypes.GET_AVAIALBLE_APARTMENTS:
+            return {... state, dataState: StateEnumApp.LOADING}
+        case ApartmentActionsTypes.GET_AVAIALBLE_APARTMENT_SUCCESS:
+            return {... state, dataState: StateEnumApp.LOADED, apartments:(<ApartmentActions>action).payload}
+        case ApartmentActionsTypes.GET_AVAIALBLE_APARTMENT_ERROR:
+            return {... state, dataState: StateEnumApp.ERROR, apartments:(<ApartmentActions>action).payload}
+
+
+        case ApartmentActionsTypes.GET_UNAVAIALBLE_APARTMENTS:
+            return {... state, dataState: StateEnumApp.LOADING}
+        case ApartmentActionsTypes.GET_UNAVAIALBLE_APARTMENT_SUCCESS:
+            return {... state, dataState: StateEnumApp.LOADED, apartments:(<ApartmentActions>action).payload}
+        case ApartmentActionsTypes.GET_UNAVAIALBLE_APARTMENT_ERROR:
+            return {... state, dataState: StateEnumApp.LOADED, apartments:(<ApartmentActions>action).payload}
+
+
+
         default : return {...state}
+
+
     } 
 }
